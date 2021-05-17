d8eb9d2fbc84c35fa39a49520c3a963ce811038c
Yaozhtj <1697661410@qq.com>
2021-05-17 19:07:19 +0800

    -yao -05.17.19 -v0.2 -修改不吃子着法生成顺序

--- src/moves_getter.cpp
+++ src/moves_getter.cpp
@@ -235,68 +235,59 @@ void GetAllNotCaptureMovements(const Situation & situation, int & num_of_all_mov
     int player_flag = GetPlayerFlag(situation.current_player), player = situation.current_player;
     int piece_from, piece_to, can_move_counter, piece_to_id;
 
-    // 2. 将(帅)的不吃子着法
-    for(int i = player_flag + 0; i <= player_flag + 0; i ++){
+    // 2. 车和炮的不吃子着法
+    for(int i = player_flag + 7; i <= player_flag + 10; i ++){
         if(situation.current_pieces[i] != 0){
             piece_from = situation.current_pieces[i];
-            can_move_counter = 0;
-            piece_to = KING_CAN_GET[piece_from][player][can_move_counter];
-            while(piece_to != 0){
-                piece_to_id = situation.current_board[piece_to];
-                if(piece_to_id == 0){
-                    all_movements[num_of_all_movements].capture = piece_to_id;
-                    all_movements[num_of_all_movements].from = piece_from;
-                    all_movements[num_of_all_movements].to = piece_to;
-                    num_of_all_movements ++;
-                }
-                can_move_counter ++;
-                piece_to = KING_CAN_GET[piece_from][player][can_move_counter];
+            int col = GetCol(piece_from), row = GetRow(piece_from);
+            // 上下方向
+            int get_row = ROOK_CANNON_CAN_GET_COL[row - 3][situation.bit_col[col]].no_capture[0];
+            while(get_row != row){
+                piece_to = GetPosition(col, get_row);
+                piece_to_id = 0;
+                all_movements[num_of_all_movements].capture = piece_to_id;
+                all_movements[num_of_all_movements].from = piece_from;
+                all_movements[num_of_all_movements].to = piece_to;
+                num_of_all_movements ++;
+                get_row --;
             }
+            get_row = ROOK_CANNON_CAN_GET_COL[row - 3][situation.bit_col[col]].no_capture[1];
+            while(get_row != row){
+                piece_to = GetPosition(col, get_row);
+                piece_to_id = 0;
+                all_movements[num_of_all_movements].capture = piece_to_id;
+                all_movements[num_of_all_movements].from = piece_from;
+                all_movements[num_of_all_movements].to = piece_to;
+                num_of_all_movements ++;
+                get_row ++;
+            }
+
+            // 左右方向
+            int get_col = ROOK_CANNON_CAN_GET_ROW[col - 3][situation.bit_row[row]].no_capture[0];
+            while(get_col != col){
+                piece_to = GetPosition(get_col, row);
+                piece_to_id = 0;
+                all_movements[num_of_all_movements].capture = piece_to_id;
+                all_movements[num_of_all_movements].from = piece_from;
+                all_movements[num_of_all_movements].to = piece_to;
+                num_of_all_movements ++;
+                get_col --;
+            }
+            get_col = ROOK_CANNON_CAN_GET_ROW[col - 3][situation.bit_row[row]].no_capture[1];
+            while(get_col != col){
+                piece_to = GetPosition(get_col, row);
+                piece_to_id = 0;
+                all_movements[num_of_all_movements].capture = piece_to_id;
+                all_movements[num_of_all_movements].from = piece_from;
+                all_movements[num_of_all_movements].to = piece_to;
+                num_of_all_movements ++;
+                get_col ++;
+            }
+
         }
     }
 
-    // 3. 士(仕)的不吃子着法
-    for(int i = player_flag + 1; i <= player_flag + 2; i ++){
-        if(situation.current_pieces[i] != 0){
-            piece_from = situation.current_pieces[i];
-            can_move_counter = 0;
-            piece_to = ADVISOR_CAN_GET[piece_from][player][can_move_counter];
-            while(piece_to != 0){
-                piece_to_id = situation.current_board[piece_to];
-                if(piece_to_id == 0){
-                    all_movements[num_of_all_movements].capture = piece_to_id;
-                    all_movements[num_of_all_movements].from = piece_from;
-                    all_movements[num_of_all_movements].to = piece_to;
-                    num_of_all_movements ++;
-                }
-                can_move_counter ++;
-                piece_to = ADVISOR_CAN_GET[piece_from][player][can_move_counter];
-            }
-        }
-    }
-    // 4. 象(相)的不吃子着法
-    for(int i = player_flag + 3; i <= player_flag + 4; i ++){
-        if(situation.current_pieces[i] != 0){
-            int bishop_eye;                  // 象眼位置
-            piece_from = situation.current_pieces[i];
-            can_move_counter = 0;
-            piece_to = BISHOP_CAN_GET[piece_from][player][can_move_counter];
-            while(piece_to != 0){
-                bishop_eye = BISHOP_EYE[piece_from][player][can_move_counter];
-                piece_to_id = situation.current_board[piece_to];
-                if(piece_to_id == 0 && situation.current_board[bishop_eye] == 0){
-                    all_movements[num_of_all_movements].capture = piece_to_id;
-                    all_movements[num_of_all_movements].from = piece_from;
-                    all_movements[num_of_all_movements].to = piece_to;
-                    num_of_all_movements ++;
-                }
-                can_move_counter ++;
-                piece_to = BISHOP_CAN_GET[piece_from][player][can_move_counter];
-            }
-        }
-    }
-
-    // 5. 马的不吃子着法
+    // 3. 马的不吃子着法
     for(int i = player_flag + 5; i <= player_flag + 6; i ++){
         if(situation.current_pieces[i] != 0){
             int horse_leg;                  // 马腿位置
@@ -318,64 +309,7 @@ void GetAllNotCaptureMovements(const Situation & situation, int & num_of_all_mov
         }
     }
 
-    // 6. 车和炮的不吃子着法
-    for(int i = player_flag + 7; i <= player_flag + 10; i ++){
-        if(situation.current_pieces[i] != 0){
-            piece_from = situation.current_pieces[i];
-            int col = GetCol(piece_from), row = GetRow(piece_from);
-
-            // 右方向
-            int get_col = ROOK_CANNON_CAN_GET_ROW[col - 3][situation.bit_row[row]].no_capture[0];
-            while(get_col != col){
-                piece_to = GetPosition(get_col, row);
-                piece_to_id = 0;
-                all_movements[num_of_all_movements].capture = piece_to_id;
-                all_movements[num_of_all_movements].from = piece_from;
-                all_movements[num_of_all_movements].to = piece_to;
-                num_of_all_movements ++;
-                get_col --;
-            }
-
-            // 左方向
-            get_col = ROOK_CANNON_CAN_GET_ROW[col - 3][situation.bit_row[row]].no_capture[1];
-            while(get_col != col){
-                piece_to = GetPosition(get_col, row);
-                piece_to_id = 0;
-                all_movements[num_of_all_movements].capture = piece_to_id;
-                all_movements[num_of_all_movements].from = piece_from;
-                all_movements[num_of_all_movements].to = piece_to;
-                num_of_all_movements ++;
-                get_col ++;
-            }
-
-            // 下方向
-            int get_row = ROOK_CANNON_CAN_GET_COL[row - 3][situation.bit_col[col]].no_capture[0];
-            while(get_row != row){
-                piece_to = GetPosition(col, get_row);
-                piece_to_id = 0;
-                all_movements[num_of_all_movements].capture = piece_to_id;
-                all_movements[num_of_all_movements].from = piece_from;
-                all_movements[num_of_all_movements].to = piece_to;
-                num_of_all_movements ++;
-                get_row --;
-            }
-
-            // 上方向
-            get_row = ROOK_CANNON_CAN_GET_COL[row - 3][situation.bit_col[col]].no_capture[1];
-            while(get_row != row){
-                piece_to = GetPosition(col, get_row);
-                piece_to_id = 0;
-                all_movements[num_of_all_movements].capture = piece_to_id;
-                all_movements[num_of_all_movements].from = piece_from;
-                all_movements[num_of_all_movements].to = piece_to;
-                num_of_all_movements ++;
-                get_row ++;
-            }
-            
-        }
-    }
-    
-    // 7. 兵(卒)的不吃子着法
+    // 4. 兵(卒)的不吃子着法
     for(int i = player_flag + 11; i <= player_flag + 15; i ++){
         if(situation.current_pieces[i] != 0){
             piece_from = situation.current_pieces[i];
@@ -391,6 +325,68 @@ void GetAllNotCaptureMovements(const Situation & situation, int & num_of_all_mov
                 }
                 can_move_counter ++;
                 piece_to = PAWN_CAN_GET[piece_from][player][can_move_counter];
+            }
+        }
+    }
+
+    // 5. 象(相)的不吃子着法
+    for(int i = player_flag + 3; i <= player_flag + 4; i ++){
+        if(situation.current_pieces[i] != 0){
+            int bishop_eye;                  // 象眼位置
+            piece_from = situation.current_pieces[i];
+            can_move_counter = 0;
+            piece_to = BISHOP_CAN_GET[piece_from][player][can_move_counter];
+            while(piece_to != 0){
+                bishop_eye = BISHOP_EYE[piece_from][player][can_move_counter];
+                piece_to_id = situation.current_board[piece_to];
+                if(piece_to_id == 0 && situation.current_board[bishop_eye] == 0){
+                    all_movements[num_of_all_movements].capture = piece_to_id;
+                    all_movements[num_of_all_movements].from = piece_from;
+                    all_movements[num_of_all_movements].to = piece_to;
+                    num_of_all_movements ++;
+                }
+                can_move_counter ++;
+                piece_to = BISHOP_CAN_GET[piece_from][player][can_move_counter];
+            }
+        }
+    }
+
+    // 6. 士(仕)的不吃子着法
+    for(int i = player_flag + 1; i <= player_flag + 2; i ++){
+        if(situation.current_pieces[i] != 0){
+            piece_from = situation.current_pieces[i];
+            can_move_counter = 0;
+            piece_to = ADVISOR_CAN_GET[piece_from][player][can_move_counter];
+            while(piece_to != 0){
+                piece_to_id = situation.current_board[piece_to];
+                if(piece_to_id == 0){
+                    all_movements[num_of_all_movements].capture = piece_to_id;
+                    all_movements[num_of_all_movements].from = piece_from;
+                    all_movements[num_of_all_movements].to = piece_to;
+                    num_of_all_movements ++;
+                }
+                can_move_counter ++;
+                piece_to = ADVISOR_CAN_GET[piece_from][player][can_move_counter];
+            }
+        }
+    }
+    
+    // 7. 将(帅)的不吃子着法
+    for(int i = player_flag + 0; i <= player_flag + 0; i ++){
+        if(situation.current_pieces[i] != 0){
+            piece_from = situation.current_pieces[i];
+            can_move_counter = 0;
+            piece_to = KING_CAN_GET[piece_from][player][can_move_counter];
+            while(piece_to != 0){
+                piece_to_id = situation.current_board[piece_to];
+                if(piece_to_id == 0){
+                    all_movements[num_of_all_movements].capture = piece_to_id;
+                    all_movements[num_of_all_movements].from = piece_from;
+                    all_movements[num_of_all_movements].to = piece_to;
+                    num_of_all_movements ++;
+                }
+                can_move_counter ++;
+                piece_to = KING_CAN_GET[piece_from][player][can_move_counter];
             }
         }
     }
